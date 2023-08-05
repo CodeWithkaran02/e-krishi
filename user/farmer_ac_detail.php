aafae4e8f98517f3a860e838b35c62d7fdd8504c
CodeWithkaran02 <7870karuna@gmail.com>
2023-08-05 22:28:07 +0530

    gramin kisan system

---
+++ user/farmer_ac_detail.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+  <title>e-krishi darshan</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link href="../style.css" rel="stylesheet" type="text/css" />
+  <link href="../layout.css" rel="stylesheet" type="text/css" />
+  <script src="../maxheight.js" type="text/javascript"></script>
+  <script src="../jquery.js" type="text/javascript"></script>
+  <script src="../js/jq.js" type="text/javascript"></script>
+  <script src="../js/easyslider.js" type="text/javascript"></script>
+  <script type="text/javascript" charset="utf-8">
+    // <![CDATA[
+    $(document).ready(function() {
+      $(".slider").easySlider({
+        controlsBefore: '<p id="controls">',
+        controlsAfter: '</p>',
+        auto: true,
+        continuous: true
+      });
+    });
+    // ]]>
+  </script>
+
+</head>
+
+<body id="page1" onload="new ElementMaxHeight();">
+
+  <div id="main">
+
+    <!-- header -->
+    <div id="header">
+      <div class="row-1">
+        <div class="wrapper">
+          <div class="logo">
+            <h1><a href="#">krishi darshan</a></h1>
+            <em><span class="txt_up">e-</span></em> <strong>A complete farmers solutions</strong>
+          </div>
+          <div class="phones"> Call Us+91-9308656390<br />
+            <span class="txt_cl">
+              <a href="../auctions/farmer_login.php">LOG IN ! ! !</a></span>
+          </div>
+        </div>
+      </div>
+
+      <div class="row-2">
+        <div class="indent">
+
+          <!-- header-box begin -->
+          <div class="header-box">
+            <!--SLIDER begin-->
+            <div class="slider">
+              <div class="slice1">
+                <div class="slice2" id="slider">
+                  <ul>
+                    <li class="slide">
+                      <div>
+                        <p class="img">
+                          <img src="../images/image31.jpg" width="483" height="272" />
+                        </p>
+                        <h2>Collection of Different crops</h2>
+                        <p>We sale all kinds of crops, With varieties of seeds and crops.</p>
+                        <p><a href="../info.html">
+                            <img src="../images/info4.gif" width="123" height="90" border="0" alt="view" /></a></p>
+                      </div>
+                    </li>
+                    <li class="slide">
+                      <div>
+                        <p class="img">
+                          <img src="../images/image29.jpg" width="483" height="272" />
+                        </p>
+                        <h2>Need help?</h2>
+                        <p>Easy to access just by clicking on registration part for e-krishi solutions</p>
+                        <p><a href="../info.html">
+                            <img src="../images/info5.gif" width="133" height="90" border="0" alt="view" /></a></p>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <!--Slider ends-->
+            <div class="inner">
+              <ul class="nav">
+                <li><a href="../index.php" class="current">Home page</a></li>
+                <li><a href="../services.php">Services</a></li>
+                <li><a href="../sale.php">Sale Crop</a></li>
+                <li><a href="../auctions/farmer_login.php">Crop Auction</a></li>
+                <li><a href="../testimonials.php">Booking</a></li>
+                <li><a href="../booking.php">About Us</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <!-- header-box end -->
+        </div>
+      </div>
+    </div>
+    <!-- content -->
+    <div id="content">
+      <div class="wrapper">
+        <div class="aside">
+          <!-- box begin -->
+          <div class="box maxheight">
+            <div class="inner">
+              &nbsp; &nbsp; &nbsp;
+              <h3>Sign In:</h3>
+              <form action="#" id="reservation-form">
+                <fieldset>
+                  <div class="field">
+                    <label>Aadhar No:</label>
+                    <input type="text" id="an" name="an" placeholder="Please enter your Aadhar number">
+                    <br />*
+                  </div>
+                  &nbsp; &nbsp; &nbsp;
+                  <div class="field">
+                    <label>Registration No:</label>
+                    <input type="text" id="reg" name="reg" placeholder="Please enter your Registration number">
+                  </div>
+                  &nbsp; &nbsp; &nbsp;
+                  <div class="field">
+                    <label>Password:</label>
+                    <input type="password" id="pass" name="pass" placeholder="Please enter your password">
+                  </div>&nbsp; &nbsp; &nbsp;
+                  <div class="button"><span><span><a href="#">LOG IN !!!</a></span></span>
+                    <br /><br />&nbsp; &nbsp; &nbsp;
+                    <h2>--------------</h2>
+                    <h3>New User</h3>
+                    <div class="button"><span><span><a href="ac_create_form.php">Create New Account!!!</a></span></span>
+                    </div>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+          <!-- box end -->
+        </div>
+        <div class="content">
+
+        </div>
+      </div>
+      <div id="right_div"></div>
+    </div>
+    <!-- footer -->
+    <div id="footer">
+      <ul class="nav">
+        <li><a href="#">Home</a>|</li>
+        <li><a href="#">Services</a>|</li>
+        <li><a href="#">Gallery</a>|</li>
+        <li><a href="#">Restaurant</a>|</li>
+        <li><a href="#">Testimonials</a>|</li>
+        <li><a href="#">Booking</a></li>
+      </ul>
+      <div class="wrapper">
+        <div class="fleft">Copyright &copy; 2023 <a href="#">Karuna Karan</a> All Rights Reserved</div>
+        <div class="fright">Designed by Karuna Karan- <a href="http://www.templatemonster.com/">Roll No. 21MCRMS97047</a> Semester IV</div>
+      </div>
+    </div>
+  </div>
+  <div align=center> Master of Computer Application, Ranchi University, Ranchi Jharkhand, INDIA</div>
+</body>
+
+</html>
